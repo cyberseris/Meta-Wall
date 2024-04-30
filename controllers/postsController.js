29304e8c0b23a20379e75f7aec06282f35cb646f
SerisKey <zihlingchen@gmail.com>
2024-04-30 13:59:09 +0800

    簡化程式
    簡化程式

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,102 +1,65 @@
 const Post = require("../models/postsModel");
-let post = "";
 
-const searchController =  async function(req, res, next) {
-    if(req.query.timeSort == "desc"){
-        const timeSort = "-createdAt";  
-        try{
-        if(typeof req.query.Keyword!=='undefined'){
-            Keyword = {"content": new RegExp(req.query.Keyword)}
-            post = await Post.find(Keyword).sort(timeSort);    
-        }else{
-            post = await Post.find().sort(timeSort);
-        } 
+const searchController = async function (req, res, next) {
+    let post = "";
+    let timeSort = "";
+    let keywordFilter = {};
+
+    if (req.query.timeSort == "desc" || req.query.timeSort == "monthDesc") {
+        timeSort = "-createdAt";
+    } else {
+        timeSort = "createdAt";
+    }
+
+    if (req.query.timeSort == "monthDesc") {
+        const today = new Date();
+        const startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
+        keywordFilter = {
+            createdAt: {
+                $gte: startOfMonth,
+            }
+        };
+    }
+
+    if (req.query.Keyword) {
+        keywordFilter.content = new RegExp(req.query.Keyword)
+    }
+
+    try {
+        post = await Post.find(keywordFilter).sort(timeSort);
+
         return res.status(200).json({
             success: true,
             message: "搜尋成功",
             post
         })
-        }catch(err){
-            res.status(400).json({
-                success: false,
-                message: "查詢異常",
-                errMsg: err.message
-              })
-        }
-    }else if(req.query.timeSort == "monthDesc"){
-        const timeSort = "-createdAt";  
-        const today = new Date();
-        const startOfMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-
-        try{
-            if(typeof req.query.Keyword!=='undefined'){
-                Keyword = {"content": new RegExp(req.query.Keyword)}
-                post = await Post.find({...Keyword,
-                    createdAt: {
-                        $gte: startOfMonth,
-                    }
-                }).sort(timeSort);  
-            }else{
-                post = await Post.find({
-                    createdAt: {
-                        $gte: startOfMonth,
-                    }
-                }).sort(timeSort);
-            }
-    
-            return res.status(200).json({
-                success: true,
-                message: "搜尋成功",
-                post
-            })  
-        }catch(err){
-            res.status(400).json({
-                success: false,
-                message: "查詢異常",
-                errMsg: err.message
-              })
-        }
-    }else{
-        try{
-            if(typeof req.query.Keyword!=='undefined'){
-                Keyword = {"content": new RegExp(req.query.Keyword)}
-                post = await Post.find(Keyword);    
-            }else{
-                post = await Post.find();
-            }
-            return res.status(200).json({
-                success: true,
-                message: "搜尋成功",
-                post
-            })
-        }catch(err){
-            res.status(400).json({
-                success: false,
-                message: "查詢異常",
-                errMsg: err.message
-              })
-        }
+    } catch (err) {
+        res.status(400).json({
+            success: false,
+            message: "查詢異常",
+            errMsg: err.message
+        });
     }
 };
 
-const postController =  async function(req, res, next) {
-    try{
+const postController = async function (req, res, next) {
+    try {
         const newPost = await Post.create(req.body);
         return res.status(200).json({
             success: true,
             message: "新增貼文成功",
             post: newPost
         })
-    }catch(err){
+    } catch (err) {
         res.status(400).json({
             success: false,
             message: "新增貼文失敗",
             errMsg: err.message
-          })
+        })
     }
-  };
+};
 
-  module.exports = {
-    searchController:searchController,
-    postController:postController
-  }
+module.exports = {
+    searchController: searchController,
+    postController: postController
+}
